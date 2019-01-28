3497f83c0236ebb6acc1458967de41937d76de15
Alex D'Amico <alex@checkmate.digital>
2019-01-28 13:26:49 -0500

    feat(bootstrap): install bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import 'normalize.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 

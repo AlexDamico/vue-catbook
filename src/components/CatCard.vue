106bf72356e725146fc70366b4b53654fad8734c
Alex D'Amico <AlexDamico@users.noreply.github.com>
2019-12-10 10:18:48 -0500

    hotfix(packages): upgrade serialize javascript and old dependencies

---
+++ src/components/CatCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="card">
+        <h1>Name</h1>
+        <p>Description</p>
+        <div class="row">
+            <div class="col-sm-6">
+                A
+            </div>
+            <div class="col-sm-6">
+                B
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="row">
+      <b-card title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2">
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </div>
   </div>
 </template>
 
